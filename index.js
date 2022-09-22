920725312967875f8aa2960adca502f040e59fc5
hrenaud (NovaGaïa) <renaud.heluin@novagaia.com>
2022-09-22 09:37:06 +0200

    feat(Babel): Add building babel capacities.

--- index.js
+++ index.js
@@ -1,5 +1,1 @@
-import Banner from './src/components/Banner'
-import RGPDBlocker from './src/components/RGPDBlocker'
-import { resetAll } from './src/utils/ls'
-
-export { Banner as RGPDCookieBanner, resetAll, RGPDBlocker }
+// noop
---
+++ src/rgpd-acceptance.js
@@ -0,0 +1,5 @@
+import Banner from './components/Banner'
+import RGPDBlocker from './components/RGPDBlocker'
+import { resetAll } from './utils/ls'
+
+export { Banner as RGPDCookieBanner, resetAll, RGPDBlocker }
